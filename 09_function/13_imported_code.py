59474c991a66afed47f25c3bec2d2a04b2eb14d0
piyushasthana123 <piyushasthana444@gmail.com>
2026-09-03 23:33:17 +0530

    new code3

---
+++ 09_function/13_imported_code.py
@@ -0,0 +1,10 @@
+# import  user_defined_modules
+# a = 100
+# b =20
+# result = user_defined_modules.add(a,b)
+# print(result) #120
+
+import  user_defined_modules
+a= 100
+output = user_defined_modules.square_root(a)
+print(f"square root of {a} is : {output}") #square root of 100 is : 10.0
