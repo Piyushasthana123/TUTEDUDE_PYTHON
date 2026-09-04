16c8e7f996d47633e3a13248029bd555caf28dc5
piyushasthana123 <piyushasthana444@gmail.com>
2026-09-04 23:54:47 +0530

    new code4

--- 09_function/13_imported_code.py
+++ 09_function/13_imported_code.py
@@ -8,3 +8,6 @@ import  user_defined_modules
 a= 100
 output = user_defined_modules.square_root(a)
 print(f"square root of {a} is : {output}") #square root of 100 is : 10.0
+"
+# for 13_imported_code.py => __n'arithematic name__=> "-__main__"
+# for arithematic.py => name =>"
--- 09_function/user_defined_modules.py
+++ 09_function/user_defined_modules.py
@@ -4,5 +4,14 @@ simple arithmetics module: arithmetics.py
 def add(num1,num2):
     return num1+num2
 def square_root(num):
-    return num**0.5
- 
+    return num ** 0.5
+# a = 10
+# b = 20
+# result = add(a,b)
+# print(result) #30
+
+if __name__ == "__main__":
+    a = 10
+    b = 20
+    result = addd(a,b)
+    print(result)
